f800349f80b92de0eed6aff9a721af2372b46fac
Márcio Diniz <marcio.sccp10@gmail.com>
2023-06-08 10:18:03 -0300

    Update index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirect</title>
+</head>
+<body onload="redirect()">
+    <script>
+        function redirect() {
+            window.location.href = 'interface.html';
+        }
+    </script>
+</body>
+</html>
